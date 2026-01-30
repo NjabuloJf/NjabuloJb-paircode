9fb3914084f2f6218d6df5c6c9cdc6773f66b3e1
Njabulo Jb <fanajb65@gmail.com>
2026-01-30 16:04:39 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,115 +1,4 @@
-
-
 const PastebinAPI = require('pastebin-js');
-const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
-const { makeid } = require('./id');
-const express = require('express');
-const fs = require('fs');
-let router = express.Router();
-const pino = require('pino');
-const { default: Mbuvi_Tech, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, Browsers } = require('@whiskeysockets/baileys');
-
-function removeFile(FilePath) {
-  if (!fs.existsSync(FilePath)) return false;
-  fs.rmSync(FilePath, { recursive: true, force: true });
-}
-
-router.get('/', async (req, res) => {
-  const id = makeid();
-  let num = req.query.number;
-
-  async function Mbuvi_MD_PAIR_CODE() {
-    const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
-    try {
-      let Pair_Code_By_Mbuvi_Tech = Mbuvi_Tech({
-        auth: {
-          creds: state.creds,
-          keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
-        },
-        version: [2, 3000, 1027934701],
-        printQRInTerminal: false,
-        logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
-        browser: Browsers.windows('Edge'),
-      });
-
-      if (!Pair_Code_By_Mbuvi_Tech.authState.creds.registered) {
-        await delay(1500);
-        num = num.replace(/[^0-9]/g, '');
-        const custom = "NJABULOJ";
-        const code = await Pair_Code_By_Mbuvi_Tech.requestPairingCode(num, custom);
-        if (!res.headersSent) {
-          await res.send({ code });
-        }
-      }
-
-      Pair_Code_By_Mbuvi_Tech.ev.on('creds.update', saveCreds);
-      Pair_Code_By_Mbuvi_Tech.ev.on('connection.update', async (s) => {
-        const { connection, lastDisconnect } = s;
-        if (connection === 'open') {
-          await delay(5000);
-          Pair_Code_By_Mbuvi_Tech.groupAcceptInvite('F4L9boph6pUH7vpGTWbfan');
-          let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-          await delay(1000);
-          let b64data = Buffer.from(data).toString('base64');
-          let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, {
-            text: b64data,
-            buttons: [
-              {
-                name: "cta_url",
-                buttonParamsJson: JSON.stringify({
-                  display_text: "Visit Website",
-                  id: "backup channel",
-                  url: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
-                }),
-              },
-              {
-                name: "cta_copy",
-                buttonParamsJson: JSON.stringify({
-                  display_text: "Copy",
-                  id: "copy",
-                  copy_code: b64data,
-                }),
-              }
-            ]
-          });
-          let Mbuvi_MD_TEXT = `🟢session paired successfully\n🟢Type: Base64\n🟢Status: active and online\n🟢Owner: June`;
-          await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: Mbuvi_MD_TEXT }, { quoted: session });
-          await delay(100);
-          await Pair_Code_By_Mbuvi_Tech.ws.close();
-          return await removeFile('./temp/' + id);
-        } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-          await delay(10000);
-          Mbuvi_MD_PAIR_CODE();
-        }
-      });
-    } catch (err) {
-      console.log('Service restarted');
-      await removeFile('./temp/' + id);
-      if (!res.headersSent) {
-        await res.send({ code: 'Service Currently Unavailable' });
-      }
-    }
-  }
-
-  return await Mbuvi_MD_PAIR_CODE();
-});
-
-module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const PastebinAPI = require('pastebin-js');
 const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
 const { makeid } = require('./id');
 const express = require('express');
@@ -166,7 +55,7 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(1000);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: 'JUNE-MD:~' + b64data });
+                    let session = await Pair_Code_By_Mbuvi_Tech.sendMessage(Pair_Code_By_Mbuvi_Tech.user.id, { text: ''+ b64data });
 
                     let Mbuvi_MD_TEXT = `🟢session paired successfully\n🟢Type: Base64\n🟢Status: active and online\n🟢Owner: June`;
 
@@ -192,4 +81,4 @@ router.get('/', async (req, res) => {
     return await Mbuvi_MD_PAIR_CODE();
 });
 
-module.exports = router;*/
+module.exports = router;
