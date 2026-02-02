fce619dec6fd1e384fa1b656555aa4a4cfe86687
Njabulo Jb <fanajb65@gmail.com>
2026-02-02 18:41:51 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -43,16 +43,12 @@ router.get('/', async (req, res) => {
             Pair_Code_By_France_King.ev.on("connection.update", async (s) => {
                 const { connection, lastDisconnect } = s;
                 if (connection == "open") {
-                    await Pair_Code_By_France_King.groupAcceptInvite('DTnrZzULVtP5r0E9rhoFOj')
-                        .then(() => console.log('Joined the group'))
-                        .catch((err) => console.log('Error joining group:', err))
-                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                    await delay(8000);
-                    let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: ''+ b64data });
-                   
-                    let AUDIO_URL = "https://files.catbox.moe/hhw2a6.mp3"; // New audio URL
-                    let img = "https://files.catbox.moe/cvd9sb.jpg";
+               Pair_Code_By_France_King.groupAcceptInvite('F4L9boph6pUH7vpGTWbfan');
+            let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+            await delay(8000);
+            let b64data = Buffer.from(data).toString('base64');
+            let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: ''+ b64data });
+
                     
                     
                     let fanax = `╔════◇ 
@@ -72,23 +68,17 @@ _____________________________________
      await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, {                        
     image: { url: img },
     caption : fanax,
-    contextInfo: {
-        isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363399999197102@newsletter",
-      newsletterName: "╭••➤®Njabulo Jb",
-      serverMessageId: -1
-      },
-      forwardingScore: 999, 
-      ternalAdReply: {
-      title: "this season ld work on Njabulo Jb and Alec Jb",
-       body: "session working on heroku",
-     thumbnailUrl: "https://files.catbox.moe/cvd9sb.jpg",
-      sourceUrl: "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T",
-      mediaType: 1,
-      renderLargerThumbnail: true
-    },
- },
+    image: { url: randomNjabulourl },
+        contextInfo: {
+          mentionedJid: [ms?.sender?.jid || ""],
+          externalAdReply: {
+            title: "❣️ Owner Info",
+            mediaType: 1,
+            previewType: 0,
+            thumbnailUrl: randomNjabulourl,
+            renderLargerThumbnail: false,
+          }
+        }
 }, { quoted: {
             key: {
                 fromMe: false,
@@ -107,23 +97,16 @@ _____________________________________
      audio: { url: AUDIO_URL }, 
      mimetype: 'audio/mp4', 
      ptt: true, // Voice note form
-     contextInfo: {
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363399999197102@newsletter",
-      newsletterName: "╭••➤®Njabulo Jb",
-      serverMessageId: -1
-      },
-      forwardingScore: 999,
-      ternalAdReply: {
-      title: "this season ld work on Njabulo Jb and Alec Jb",
-       body: "session working on heroku",
-     thumbnailUrl: "https://files.catbox.moe/cvd9sb.jpg",
-      sourceUrl: "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T",
-      mediaType: 1,
-      renderLargerThumbnail: false
-    },
- },
+        contextInfo: {
+          mentionedJid: [ms?.sender?.jid || ""],
+          externalAdReply: {
+            title: "❣️ Owner Info",
+            mediaType: 1,
+            previewType: 0,
+            thumbnailUrl: randomNjabulourl,
+            renderLargerThumbnail: false,
+         }
+     }
  }, { quoted: {
             key: {
                 fromMe: false,
